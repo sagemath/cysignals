d3eecf04f9252d83d16908a5f04ed2ddc1c83ca5
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2018-03-09 11:01:07 +0100

    Fix example when Cython was not installed

--- Makefile
+++ Makefile
@@ -47,7 +47,7 @@ clean-doc:
 	rm -rf docs/build
 
 distclean: clean
-	rm -rf .eggs
+	rm -rf .eggs example/.eggs
 	rm -rf autom4te.cache
 	rm -f config.log config.status
 
@@ -107,7 +107,7 @@ check-user: check-user-doctest check-user-example
 
 user-install: configure
 	rm -rf tmp/user
-	export PYTHONUSERBASE="`pwd`/tmp/user" && $(PYTHON) setup.py install --user --single-version-externally-managed --record=/dev/null
+	export PYTHONUSERBASE="`pwd`/tmp/user" && $(PYTHON) setup.py install --user --old-and-unmanageable
 
 check-user-doctest: user-install
 	export PYTHONUSERBASE="`pwd`/tmp/user" && $(DOCTEST) src/cysignals/*.pyx
--- example/setup.py
+++ example/setup.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from Cython.Build import cythonize
-import sys
+from setuptools import setup
+from distutils.command.build import build as _build
 
-extensions=cythonize("cysignals_example.pyx")
 
-# Run Distutils
+class build(_build):
+    def run(self):
+        dist = self.distribution
+        ext_modules = dist.ext_modules
+        if ext_modules:
+            dist.ext_modules[:] = self.cythonize(ext_modules)
+        _build.run(self)
+
+    def cythonize(self, extensions):
+        from Cython.Build.Dependencies import cythonize
+        return cythonize(extensions)
+
+
 setup(
     name="cysignals_example",
     version='1.0',
-    ext_modules=extensions,
-    license='GNU Lesser General Public License, version 3 or later',
+    license='Public Domain',
+    setup_requires=["Cython"],
+    ext_modules=["cysignals_example.pyx"],
+    cmdclass=dict(build=build),
 )

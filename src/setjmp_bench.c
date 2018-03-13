b086b2874ff3aa1c236a35dfacf29198a1376176
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2018-03-13 10:21:34 +0100

    Clean up bench program

--- src/setjmp_bench.c
+++ src/setjmp_bench.c
@@ -1,53 +1,35 @@
-#undef _FORTIFY_SOURCE
-#undef _FORTIFY_SOURCE
-#undef _FORTIFY_SOURCE
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <setjmp.h>
 #include <sys/time.h>
+
 
 static jmp_buf env;
 static sigjmp_buf sigenv;
 
+
+#define BENCH(CODE) \
+    gettimeofday(&tv0, NULL); \
+    for (i = 0; i < N; i++) {CODE; asm("");} \
+    gettimeofday(&tv1, NULL); \
+    ns = (double)(tv1.tv_sec - tv0.tv_sec) * 1e9 + \
+         (double)(tv1.tv_usec - tv0.tv_usec) * 1e3; \
+    ns /= (double)N;
+
+
 int main(int argc, char** argv)
 {
-    unsigned int i, N = 10000000;
+    long i, N = 10000000;
+    if (argc >= 2) N = atol(argv[1]);
     struct timeval tv0, tv1;
-    double t;
+    double ns;
 
-    gettimeofday(&tv0, NULL);
-    for (i = 0; i < N; i++)
-    {
-        if (setjmp(env)) return 0;
-        asm("");
-    }
-    gettimeofday(&tv1, NULL);
-    
-    t = (double)(tv1.tv_sec - tv0.tv_sec) * 1e0;
-    t += (double)(tv1.tv_usec - tv0.tv_usec) * 1e-6;
-    printf("Time per iteration for setjmp():    %8.2fns\n", 1e9 * t/(double)N);
+    BENCH(if (setjmp(env)) return 0)
+    printf("Time for setjmp(env):      %8.2fns\n", ns);
 
-    gettimeofday(&tv0, NULL);
-    for (i = 0; i < N; i++)
-    {
-        if (sigsetjmp(env, 0)) return 0;
-        asm("");
-    }
-    gettimeofday(&tv1, NULL);
-    
-    t = (double)(tv1.tv_sec - tv0.tv_sec) * 1e0;
-    t += (double)(tv1.tv_usec - tv0.tv_usec) * 1e-6;
-    printf("Time per iteration for sigsetjmp(0):%8.2fns\n", 1e9 * t/(double)N);
+    BENCH(if (sigsetjmp(env, 0)) return 0)
+    printf("Time for sigsetjmp(env, 0):%8.2fns\n", ns);
 
-    gettimeofday(&tv0, NULL);
-    for (i = 0; i < N; i++)
-    {
-        if (sigsetjmp(env, 1)) return 0;
-        asm("");
-    }
-    gettimeofday(&tv1, NULL);
-    
-    t = (double)(tv1.tv_sec - tv0.tv_sec) * 1e0;
-    t += (double)(tv1.tv_usec - tv0.tv_usec) * 1e-6;
-    printf("Time per iteration for sigsetjmp(1):%8.2fns\n", 1e9 * t/(double)N);
+    BENCH(if (sigsetjmp(env, 1)) return 0)
+    printf("Time for sigsetjmp(env, 1):%8.2fns\n", ns);
 }

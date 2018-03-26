b9fbcaea090876b39a51449279c7ba2a6096edad
Erik M. Bray <erik.bray@lri.fr>
2018-03-26 13:53:55 +0200

    add additional VectoredContinueHandler on Cygwin that would be reached when handling an exception occurring during signal handling on an alternate stack
    as on other systems, this basically just prints an error and exits with
    an appropriate return code (128 + signal number); it also attempts to
    print a stack trace but this currently fails on Cygwin, and we can live
    with that for now

--- src/cysignals/implementation.c
+++ src/cysignals/implementation.c
@@ -162,6 +162,12 @@ static inline void sigdie_for_sig(int sig, int inside)
 }
 
 
+/* Additional platform-specific implementation code */
+#if defined(__CYGWIN__) && defined(__x86_64__)
+#include "implementation_cygwin.h"
+#endif
+
+
 /* Handler for SIGHUP, SIGINT, SIGALRM
  *
  * Inside sig_on() (i.e. when cysigs.sig_on_count is positive), this
@@ -406,6 +412,7 @@ static void setup_alt_stack(void)
     ss.ss_size = sizeof(alt_stack);
     ss.ss_flags = 0;
     if (sigaltstack(&ss, NULL) == -1) {perror("sigaltstack"); exit(1);}
+    PLATFORM_SETUP_ALT_STACK;
 }
 
 
---
+++ src/cysignals/implementation_cygwin.h
@@ -0,0 +1,61 @@
+/* Cygwin-specific implementation details */
+
+#if defined(__CYGWIN__) && defined(__x86_64__)
+#include <w32api/errhandlingapi.h>
+LONG WINAPI win32_altstack_handler(EXCEPTION_POINTERS *exc)
+{
+    int sig = 0;
+    /* Logic cribbed from Cygwin for mapping common Windows exception
+     * codes to the relevant signal numbers:
+     * https://cygwin.com/git/gitweb.cgi?p=newlib-cygwin.git;a=blob;f=winsup/cygwin/exceptions.cc;h=77eff05707f95f7277974fadbccf0e74223d8d1c;hb=HEAD#l650
+     * Unfortunately there is no external API by which to access this
+     * mapping (a la cygwin_internal(CW_GET_ERRNO_FROM_WINERROR, ...)) */
+    switch (exc->ExceptionRecord->ExceptionCode) {
+        case STATUS_FLOAT_DENORMAL_OPERAND:
+        case STATUS_FLOAT_DIVIDE_BY_ZERO:
+        case STATUS_FLOAT_INVALID_OPERATION:
+        case STATUS_FLOAT_STACK_CHECK:
+        case STATUS_FLOAT_INEXACT_RESULT:
+        case STATUS_FLOAT_OVERFLOW:
+        case STATUS_FLOAT_UNDERFLOW:
+        case STATUS_INTEGER_DIVIDE_BY_ZERO:
+        case STATUS_INTEGER_OVERFLOW:
+            sig = SIGFPE;
+            break;
+        case STATUS_ILLEGAL_INSTRUCTION:
+        case STATUS_PRIVILEGED_INSTRUCTION:
+        case STATUS_NONCONTINUABLE_EXCEPTION:
+            sig = SIGILL;
+            break;
+        case STATUS_TIMEOUT:
+            sig = SIGALRM;
+            break;
+        case STATUS_GUARD_PAGE_VIOLATION:
+        case STATUS_DATATYPE_MISALIGNMENT:
+            sig = SIGBUS;
+            break;
+        case STATUS_ACCESS_VIOLATION:
+            /* In the case of this last resort exception handling we can
+             * probably safely assume this should be a SIGSEGV;
+             * other access violations would have already been handled by
+             * Cygwin before we wound up on the alternate stack */
+        case STATUS_STACK_OVERFLOW:
+        case STATUS_ARRAY_BOUNDS_EXCEEDED:
+        case STATUS_IN_PAGE_ERROR:
+        case STATUS_NO_MEMORY:
+        case STATUS_INVALID_DISPOSITION:
+            sig = SIGSEGV;
+            break;
+        case STATUS_CONTROL_C_EXIT:
+            sig = SIGINT;
+            break;
+    }
+
+    sig_reset_defaults();
+    sigdie_for_sig(sig, 1);
+    return ExceptionContinueExecution;
+}
+
+#undef PLATFORM_SETUP_ALT_STACK
+#define PLATFORM_SETUP_ALT_STACK AddVectoredContinueHandler(0, win32_altstack_handler)
+#endif  /* CYGWIN && __x86_64__ */

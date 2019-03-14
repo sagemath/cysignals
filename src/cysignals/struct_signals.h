807380374ac5c2a956fe92ae2aed49a4692e6d8f
Jeroen Demeyer <J.Demeyer@UGent.be>
2019-03-14 10:34:48 +0100

    Use _Atomic if possible

--- src/cysignals/struct_signals.h
+++ src/cysignals/struct_signals.h
@@ -25,6 +25,7 @@
 #include "cysignals_config.h"
 #include <setjmp.h>
 #include <signal.h>
+#include <Python.h>
 
 
 /* Choose sigjmp/longjmp variant */
@@ -39,17 +40,15 @@
 #endif
 
 
-/* Define an atomic_int type for atomic operations */
-#if CYSIGNALS_C_STDATOMIC_H || CYSIGNALS_CXX_STDATOMIC_H
-/* This header defines atomic_int */
-#include <stdatomic.h>
-#elif CYSIGNALS_CXX_ATOMIC
-/* This header defines std::atomic_int */
+/* Define a cy_atomic_int type for atomic operations */
+#if CYSIGNALS_C_ATOMIC || CYSIGNALS_CXX_ATOMIC
+typedef volatile _Atomic int cy_atomic_int;
+#elif CYSIGNALS_STD_ATOMIC
 #include <atomic>
-using std::atomic_int;
+typedef volatile std::atomic<int> cy_atomic_int;
 #else
 /* The type sig_atomic_t is not really atomic, but it's the best we have */
-typedef sig_atomic_t atomic_int;
+typedef volatile sig_atomic_t cy_atomic_int;
 #endif
 
 
@@ -58,7 +57,7 @@ typedef struct
 {
     /* Reference counter for sig_on().
      * If this is strictly positive, we are inside a sig_on(). */
-    volatile atomic_int sig_on_count;
+    cy_atomic_int sig_on_count;
 
     /* If this is nonzero, it is a signal number of a non-critical
      * signal (e.g. SIGINT) which happened during a time when it could
@@ -66,18 +65,18 @@ typedef struct
      * outside of sig_on() or inside sig_block().  To avoid race
      * conditions, this value may only be changed when all
      * interrupt-like signals are masked. */
-    volatile atomic_int interrupt_received;
+    cy_atomic_int interrupt_received;
 
     /* Are we currently handling a signal inside cysigs_signal_handler()?
      * This is set to 1 on entry in cysigs_signal_handler (not in
      * cysigs_interrupt_handler) and 0 in _sig_on_postjmp.  This is
      * needed to check for signals raised within the signal handler. */
-    volatile atomic_int inside_signal_handler;
+    cy_atomic_int inside_signal_handler;
 
     /* Non-zero if we currently are in a function such as malloc()
      * which blocks interrupts, zero normally.
      * See sig_block(), sig_unblock(). */
-    volatile atomic_int block_sigint;
+    cy_atomic_int block_sigint;
 
     /* A jump buffer holding where to cylongjmp() after a signal has
      * been received. This is set by sig_on(). */

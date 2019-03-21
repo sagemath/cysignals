84cf70f6d5a9f25ce9e4741fc071d749f437cd77
Jeroen Demeyer <J.Demeyer@UGent.be>
2019-03-21 12:02:28 +0100

    Improve portability of setjmp_bench

--- src/setjmp_bench.c
+++ src/setjmp_bench.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 1
 #include <stdio.h>
 #include <stdlib.h>
 #include <setjmp.h>
@@ -19,25 +20,19 @@ struct cyjmp_struct
 static inline int
 cysetjmp(struct cyjmp_struct* env)
 {
-    int res;
-    asm goto("\n"
-        "\tleaq %l1(%%rip), %%rcx\n"
+    __asm__ volatile("\n"
+        "\tleaq cysetjmp%=(%%rip), %%rcx\n"
         "\tmovq %%rsp, 0(%0)\n"
         "\tmovq %%rbp, 8(%0)\n"
         "\tmovq %%rcx, 16(%0)\n"
+        "cysetjmp%=:\n"
     :
     : "b" (env)
-    : /* Clobber all registers except for rdx, rbx, rsp, rbp */
+    : /* Clobber all registers except for rbx, rsp, rbp */
       "%rax", "%rcx", "%rdx", "%rsi", "%rdi",
       "%r8", "%r9", "%r10", "%r11", "%r12", "%r13", "%r14", "%r15",
-      "cc", "memory"
-    : res_in_rdx);
-
+      "cc", "memory");
     return 0;
-
-res_in_rdx:
-    asm volatile("": "=d" (res));
-    return res;
 }
 
 static struct cyjmp_struct cyenv;
@@ -46,7 +41,7 @@ static struct cyjmp_struct cyenv;
 
 #define BENCH(CODE) \
     gettimeofday(&tv0, NULL); \
-    for (i = 0; i < N; i++) {CODE; asm("");} \
+    for (i = 0; i < N; i++) {CODE; __asm__("");} \
     gettimeofday(&tv1, NULL); \
     ns = (double)(tv1.tv_sec - tv0.tv_sec) * 1e9 + \
          (double)(tv1.tv_usec - tv0.tv_usec) * 1e3; \
@@ -61,16 +56,16 @@ int main(int argc, char** argv)
     double ns;
 
     BENCH(if (setjmp(env)) return 0)
-    printf("Time for setjmp(env):      %8.2fns\n", ns);
+    printf("Time for setjmp(env):       %8.2fns\n", ns);
 
     BENCH(if (sigsetjmp(sigenv, 0)) return 0)
-    printf("Time for sigsetjmp(env, 0):%8.2fns\n", ns);
+    printf("Time for sigsetjmp(env, 0): %8.2fns\n", ns);
 
     BENCH(if (sigsetjmp(sigenv, 1)) return 0)
-    printf("Time for sigsetjmp(env, 1):%8.2fns\n", ns);
+    printf("Time for sigsetjmp(env, 1): %8.2fns\n", ns);
 
 #if __x86_64__
     BENCH(if (cysetjmp(&cyenv)) return 0)
-    printf("Time for asm implementation:%7.2fns\n", ns);
+    printf("Time for asm implementation:%8.2fns\n", ns);
 #endif
 }

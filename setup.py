6ea5a3ac1bbaffcad9dd2f96068178378a4c49eb
J08nY <johny@neuromancer.sk>
2023-05-17 17:39:58 +0200

    Suppress fortify source globally.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ kwds = dict(include_dirs=[opj("src"),
             depends=depends,
             define_macros=macros,
             undef_macros=undef_macros,
-            extra_compile_args=["-pthread"],
+            extra_compile_args=["-pthread", "-Wp,-U_FORTIFY_SOURCE"],
             extra_link_args=["-pthread"],)
 
 extensions = [

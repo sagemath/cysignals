7979a93888b6123c3a56b75cf35d9784dcb57034
Tobias Diez <code@tobiasdiez.com>
2024-10-23 15:03:00 +0200

    Update build system to use Meson

--- example/setup.py
+++ example/setup.py
@@ -1,27 +1,11 @@
-#!/usr/bin/env python3
+# meson.build
 
-from setuptools import setup
-from setuptools.extension import Extension
-from distutils.command.build import build as _build
+project('cysignals_example', 'python')
 
+py3_mod = import('python').find_installation('python3')
 
-class build(_build):
-    def run(self):
-        dist = self.distribution
-        ext_modules = dist.ext_modules
-        if ext_modules:
-            dist.ext_modules[:] = self.cythonize(ext_modules)
-        super().run()
-
-    def cythonize(self, extensions):
-        from Cython.Build.Dependencies import cythonize
-        return cythonize(extensions)
-
-
-setup(
-    name="cysignals_example",
-    version='1.0',
-    license='Public Domain',
-    ext_modules=[Extension("*", ["cysignals_example.pyx"])],
-    cmdclass=dict(build=build),
+py3_mod.extension_module('cysignals_example',
+  sources: ['cysignals_example.pyx'],
+  install: true,
+  subdir: 'cysignals_example'
 )

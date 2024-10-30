51ef87e226aa647d0f38edfc912b64e4d81dbf95
Tobias Diez <code@tobiasdiez.de>
2024-10-30 23:05:10 +0800

    fix tests

--- conftest.py
+++ conftest.py
@@ -5,13 +5,18 @@ from _pytest.doctest import DoctestModule
 
 collect_ignore = ["src/scripts/cysignals-CSI-helper.py"]
 
-"""Collect doctests in cython files and run them as test modules."""
+
 def pytest_collect_file(
     file_path: pathlib.Path,
     parent: Collector,
 ) -> DoctestModule | None:
+    """Collect doctests in cython files and run them as test modules."""
     config = parent.config
     if file_path.suffix == ".pyx":
         if config.option.doctestmodules:
             return DoctestModule.from_parent(parent, path=file_path)
     return None
+
+
+# Need to import cysignals to initialize it
+import cysignals

36844ad0d93ff25b1f1258115048d13874e7236b
Gonzalo Tornaría <tornaria@cmat.edu.uy>
2025-01-14 00:48:54 -0300

    skip new test on windows (needs cysignals.alarm)

--- tests/test_custom_signals.py
+++ tests/test_custom_signals.py
@@ -1,17 +1,23 @@
-import pytest
+"""
+Tests for custom signals.
+"""
+
 import time
+import pytest
 
 def test_clear_pending():
+    """
+    Regression test for https://github.com/sagemath/cysignals/pull/216
+    """
 
-    from cysignals.alarm import alarm, AlarmInterrupt
-
+    alarm = pytest.importorskip("cysignals.alarm")  # n/a on windows
     cypari2 = pytest.importorskip("cypari2")
 
-    with pytest.raises(AlarmInterrupt):
-        alarm(0.5)
+    with pytest.raises(alarm.AlarmInterrupt):
+        alarm.alarm(0.01)
         time.sleep(1)
 
     try:
         cypari2.Pari()
-    except AlarmInterrupt:
+    except alarm.AlarmInterrupt:
         pytest.fail("AlarmInterrupt was not cleared")

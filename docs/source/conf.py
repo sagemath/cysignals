e6feb6f4b0671774e6ef8026644b1deec7131f43
Dima Pasechnik <dima@pasechnik.info>
2025-01-25 11:39:59 -0600

    create 1.12.3, and fix building docs
    after VERSION file was removed, it stopped working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,10 @@ copyright = '2016, Martin Albrecht, Jeroen Demeyer'
 # built documents.
 #
 # The short X.Y version.
-version = open("../../VERSION").read().strip()
+import tomli
+with open("../../pyproject.toml", "rb") as f:
+    toml_dict = tomli.load(f)
+version = toml_dict['project']['version']
 # The full version, including alpha/beta/rc tags.
 release = version
 
